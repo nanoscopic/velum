fa98b1442f48af17fdeaf16ec2982874cca1e611
David Helkowski <dhelkowski@suse.com>
2018-06-28 10:43:24 -0700

    Dex OIDC connector support; ui, models, db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530105000) do
+ActiveRecord::Schema.define(version: 20180626140000) do
 
   create_table "certificate_services", force: :cascade do |t|
     t.integer  "certificate_id", limit: 4
@@ -189,6 +189,17 @@ ActiveRecord::Schema.define(version: 20180530105000) do
     t.string   "group_attr_user",    limit: 255, default: "uid"
     t.string   "group_attr_group",   limit: 255, default: "member"
     t.string   "group_attr_name",    limit: 255, default: "name"
+  end
+
+  create_table "dex_connectors_oidc", force: :cascade do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name",               limit: 255
+    t.string   "provider_url",       limit: 255
+    t.string   "client_id",          limit: 255
+    t.string   "client_secret",      limit: 255
+    t.string   "callback_url",       limit: 255, default: "http://127.0.0.1:5556/callback"
+    t.boolean  "basic_auth",                     default: true
   end
 
   add_index "dex_connectors_ldap", ["id"], name: "index_dex_connectors_ldap_on_id", unique: true, using: :btree
